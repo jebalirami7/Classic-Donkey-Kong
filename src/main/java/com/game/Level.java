b13e6ede4ac0289a580ae87ae403debfb0a7ae57
Rami Jebali <jebalirami7@gmail.com>
2023-11-18 14:45:39 +0100

    Level class updates

--- src/main/java/com/game/Level.java
+++ src/main/java/com/game/Level.java
@@ -7,18 +7,34 @@ import javafx.scene.Group;
 
 public class Level {
     
-    private List<List<Integer>> bridges;
-    private ArrayList<Bridge> bridge_objs;
+    private String name;
+    private int timeLimit;
 
-    public Level(List<List<Integer>> bridges) {
+    private List<List<Integer>> bridges;
+    private ArrayList<Bridge> bridge_objs = new ArrayList<Bridge>();
+
+    private List<List<Integer>> ladders;
+    private ArrayList<Ladder> ladder_objs = new ArrayList<Ladder>();
+
+    public Level(String name, int timeLimit, List<List<Integer>> bridges, List<List<Integer>> ladders) {
+        this.name = name;
+        this.timeLimit = timeLimit;
         this.bridges = bridges;
-        this.bridge_objs = new ArrayList<Bridge>();
+        this.ladders = ladders;
     }
 
-    void create(Group root) {
+    ArrayList<Bridge> createBridges(Group root) {
         for(int i=0; i<bridges.size(); i++) {
             bridge_objs.add(new Bridge(bridges.get(i).get(0), bridges.get(i).get(1), bridges.get(i).get(2), root));
         }
+        return bridge_objs;
+    }
+
+    ArrayList<Ladder> createLadders(Group root) {
+        for(int i=0; i<ladders.size(); i++) {
+            ladder_objs.add(new Ladder(ladders.get(i).get(0), ladders.get(i).get(1), ladders.get(i).get(2), root));
+        }
+        return ladder_objs;
     }
 
 }
