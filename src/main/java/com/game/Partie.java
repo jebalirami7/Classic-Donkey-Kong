40384155327a14baca898ea3bf4e88a6f6e03d87
Rami Jebali <jebalirami7@gmail.com>
2023-11-18 14:46:21 +0100

    Create Partie Class

---
+++ src/main/java/com/game/Partie.java
@@ -0,0 +1,139 @@
+package main.java.com.game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.Group;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.Rectangle;
+
+public class Partie {
+    // Initialize bridges rows
+    private int start_y = App.height - 2 * App.section_height;
+    private int row2_y = start_y - 4 * App.section_height;
+    private int row3_y = row2_y - 7 * App.slope - 3 * App.section_height;
+    private int row4_y = row3_y - 4 * App.section_height;
+    private int row5_y = row4_y - 7 * App.slope - 3 * App.section_height;
+    private int row6_y = row5_y - 4 * App.section_height;
+    private int row6_top = row6_y - 4 * App.slope;
+    private int row5_top = row5_y - 8 * App.slope;
+    private int row4_top = row4_y - 8 * App.slope;
+    private int row3_top = row3_y - 8 * App.slope;
+    private int row2_top = row2_y - 8 * App.slope;
+    private int row1_top = start_y - 5 * App.slope;
+
+    private boolean fireBall = false;
+
+    private int score;
+    private int attempts;
+
+    List<Level> levels;
+
+    private final int barrelSpawnTime = 3600;
+    private int barrelCount = barrelSpawnTime / 2;
+    private ArrayList<Bridge> bridge_objs = new ArrayList<Bridge>();
+    private ArrayList<Ladder> ladder_objs = new ArrayList<Ladder>();
+    private List<Barrel> barrels = new ArrayList<>();
+
+    private Rectangle oilDrum = new Rectangle(1,1);
+
+
+    public Partie() {
+        this.score = 0;
+        this.attempts = 3;
+        this.levels = new ArrayList<Level>();
+    }
+
+    void createPartie(Group root, GraphicsContext gc) {
+        
+        // Create level 1 
+        Level lvl1 = new Level("LEVEL 1", 120, 
+            List.of(
+                List.of(1, start_y, 15), List.of(16, start_y - App.slope, 3),
+                List.of(19, start_y - 2 * App.slope, 3), List.of(22, start_y - 3 * App.slope, 3),
+                List.of(25, start_y - 4 * App.slope, 3), List.of(28, start_y - 5 * App.slope, 3),
+                List.of(25, row2_y, 3), List.of(22, row2_y - App.slope, 3),
+                List.of(19, row2_y - 2 * App.slope, 3), List.of(16, row2_y - 3 * App.slope, 3),
+                List.of(13, row2_y - 4 * App.slope, 3), List.of(10, row2_y - 5 * App.slope, 3),
+                List.of(7, row2_y - 6 * App.slope, 3), List.of(4, row2_y - 7 * App.slope, 3),
+                List.of(2, row2_y - 8 * App.slope, 2), List.of(4, row3_y, 3),
+                List.of(7, row3_y - App.slope, 3), List.of(10, row3_y - 2 * App.slope, 3),
+                List.of(13, row3_y - 3 * App.slope, 3), List.of(16, row3_y - 4 * App.slope, 3),
+                List.of(19, row3_y - 5 * App.slope, 3), List.of(22, row3_y - 6 * App.slope, 3),
+                List.of(25, row3_y - 7 * App.slope, 3), List.of(28, row3_y - 8 * App.slope, 2),
+                List.of(25, row4_y, 3), List.of(22, row4_y - App.slope, 3),
+                List.of(19, row4_y - 2 * App.slope, 3), List.of(16, row4_y - 3 * App.slope, 3),
+                List.of(13, row4_y - 4 * App.slope, 3), List.of(10, row4_y - 5 * App.slope, 3),
+                List.of(7, row4_y - 6 * App.slope, 3), List.of(4, row4_y - 7 * App.slope, 3),
+                List.of(2, row4_y - 8 * App.slope, 2), List.of(4, row5_y, 3),
+                List.of(7, row5_y - App.slope, 3), List.of(10, row5_y - 2 * App.slope, 3),
+                List.of(13, row5_y - 3 * App.slope, 3), List.of(16, row5_y - 4 * App.slope, 3),
+                List.of(19, row5_y - 5 * App.slope, 3), List.of(22, row5_y - 6 * App.slope, 3),
+                List.of(25, row5_y - 7 * App.slope, 3), List.of(28, row5_y - 8 * App.slope, 2),
+                List.of(25, row6_y, 3), List.of(22, row6_y - App.slope, 3),
+                List.of(19, row6_y - 2 * App.slope, 3), List.of(16, row6_y - 3 * App.slope, 3),
+                List.of(2, row6_y - 4 * App.slope, 14), List.of(13, row6_y - 4 * App.section_height, 6),
+                List.of(10, row6_y - 3 * App.section_height, 3)
+            ),
+            List.of(
+                List.of(12, row2_y + 6 * App.slope, 2), List.of(12, row2_y + 26 * App.slope, 2),
+                List.of(25, row2_y + 11 * App.slope, 4), List.of(6, row3_y + 11 * App.slope, 3),
+                List.of(14, row3_y + 8 * App.slope, 4), List.of(10, row4_y + 6 * App.slope, 1),
+                List.of(10, row4_y + 24 * App.slope, 2), List.of(16, row4_y + 6 * App.slope, 5),
+                List.of(25, row4_y + 9 * App.slope, 4), List.of(6, row5_y + 11 * App.slope, 3),
+                List.of(11, row5_y + 8 * App.slope, 4), List.of(23, row5_y + 4 * App.slope, 1),
+                List.of(23, row5_y + 24 * App.slope, 2), List.of(25, row6_y + 9 * App.slope, 4),
+                List.of(13, row6_y + 5 * App.slope, 2), List.of(13, row6_y + 25 * App.slope, 2),
+                List.of(18, row6_y - 27 * App.slope, 4), List.of(12, row6_y - 17 * App.slope, 2),
+                List.of(10, row6_y - 17 * App.slope, 2), List.of(12, -5, 13), List.of(10, -5, 13)
+            )
+        );
+
+        ladder_objs = lvl1.createLadders(root);
+        bridge_objs = lvl1.createBridges(root);
+
+        AnimationTimer gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                updateGame(root);
+                renderGame(gc);
+            }
+        };
+        gameLoop.start();
+    }
+
+    private void updateGame(Group root) {
+        if (barrelCount < barrelSpawnTime) {
+            barrelCount++;
+        } else {
+            barrelCount = new Random().nextInt(360, 1200);
+            Barrel barrel = new Barrel(270, 260, root);
+            barrels.add(barrel);
+        }
+
+        for (Barrel barrel : barrels) {
+            barrel.checkFall(ladder_objs);
+            fireBall = barrel.update(bridge_objs, row1_top, row2_top, row3_top, row4_top, row5_top, oilDrum, fireBall);
+        }
+    }
+
+    private void renderGame(GraphicsContext gc) {
+        gc.setFill(javafx.scene.paint.Color.BLACK);
+        gc.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
+
+        for (Barrel barrel : barrels) {
+            barrel.draw();
+        }
+    }
+
+    public Rectangle drawOil() {
+        double x_coord = 4 * App.section_width;
+        double y_coord = 4.5 * App.section_height;
+        Rectangle oil = new Rectangle(x_coord, y_coord, 2 * App.section_width, 2.5 * App.section_height);
+        
+        return oil;
+    }
+    
+}
