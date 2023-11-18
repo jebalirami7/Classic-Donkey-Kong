3f64bae84d92d9b44cd5f379c1de119763f5124b
Rami Jebali <jebalirami7@gmail.com>
2023-11-18 14:44:58 +0100

    Bridge class updates

--- src/main/java/com/game/Bridge.java
+++ src/main/java/com/game/Bridge.java
@@ -7,13 +7,13 @@ import javafx.scene.shape.Rectangle;
 
 public class Bridge {
     
-    private int x_pos;
-    private int y_pos;
-    private int length;
+    private double x_pos;
+    private double y_pos;
+    private double length;
     private Rectangle top;
 
 
-    public Bridge(int x_pos, int y_pos, int length, Group root) {
+    public Bridge(double x_pos, double y_pos, double length, Group root) {
         this.x_pos = x_pos * App.section_width;
         this.y_pos = y_pos;
         this.length = length;
@@ -22,38 +22,40 @@ public class Bridge {
 
 
     private Rectangle draw(Group root) {
-        int line_width = 7;
+        int line_width = 6;
         Color color = Color.rgb(225, 51, 129);
 
         for(int i=0; i<this.length; i++) {
-            int bot_coor = this.y_pos + App.section_width;
-            int left_coor = this.x_pos + (App.section_width * i);
-            int mid_coor = left_coor + (App.section_width / 2);
-            int rigth_coor = left_coor + App.section_width;
-            int top_coor = this.y_pos;
+            double bot_coord = this.y_pos + App.section_width;
+            double left_coord = this.x_pos + (App.section_width * i);
+            double mid_coord = left_coord + (App.section_width / 2);
+            double right_coord = left_coord + App.section_width;
+            double top_coord = this.y_pos;
             
-            // Create lines
-            Line line1 = new Line(left_coor, top_coor, rigth_coor, top_coor);
+            // Create 4 lines
+            Line line1 = new Line(left_coord, top_coord, right_coord, top_coord);
             line1.setStroke(color); 
             line1.setStrokeWidth(line_width);
 
-            Line line2 = new Line(left_coor, bot_coor, rigth_coor, bot_coor);
+            Line line2 = new Line(left_coord, bot_coord, right_coord, bot_coord);
             line2.setStroke(color); 
             line2.setStrokeWidth(line_width);
 
-            Line line3 = new Line(left_coor, bot_coor, mid_coor, top_coor);
+            Line line3 = new Line(left_coord, bot_coord, mid_coord, top_coord);
             line3.setStroke(color); 
             line3.setStrokeWidth(line_width);
 
-            Line line4 = new Line(mid_coor, top_coor, rigth_coor, bot_coor);
+            Line line4 = new Line(mid_coord, top_coord, right_coord, bot_coord);
             line4.setStroke(color); 
             line4.setStrokeWidth(line_width);
 
-            // Add the new Line to the existing scene
+            // Add the new lines to the existing scene
             root.getChildren().addAll(line1, line2, line3, line4);
         }
+
+        // Create Top Line 
         Rectangle topLine = new Rectangle(x_pos, y_pos, this.length * App.section_width, 2);
-        topLine.setFill(Color.BLUE);
+        topLine.setFill(Color.TRANSPARENT);   // change color to ckeck its position
 
         root.getChildren().add(topLine);
 
