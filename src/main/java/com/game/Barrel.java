ab0a0d29c4634e15022ea1c13504cdf7443e66d8
Rami Jebali <jebalirami7@gmail.com>
2023-11-29 20:17:07 +0100

    Barrel class updates

--- src/main/java/com/game/Barrel.java
+++ src/main/java/com/game/Barrel.java
@@ -3,18 +3,22 @@ package main.java.com.game;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.Group;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class Barrel extends Pane {
     private ImageView barrel;
     private double barrelWidth = 50;
     private double barrelHeight = 50;
-    private double x_change;
-    private double y_change;
+    private double x_change = 0;
+    private double y_change = 0;
     private double pos = 0;
     private int count = 0;
     private boolean oilCollision = false;
@@ -31,90 +35,120 @@ public class Barrel extends Pane {
         barrel.setX(x);
         barrel.setY(y);
         root.getChildren().add(barrel);
-        this.bottom = new Rectangle(barrelWidth, barrelHeight);
-        this.bottom.setLayoutX(x);
-        this.bottom.setLayoutY(y);
-        this.bottom.setFill(Color.TRANSPARENT);
-        root.getChildren().add(this.bottom);
+        bottom = new Rectangle(barrelWidth, barrelHeight);
+        bottom.setLayoutX(x);
+        bottom.setLayoutY(y);
+        bottom.setFill(Color.TRANSPARENT);
+        root.getChildren().add(bottom);
     }
 
-    public boolean update(ArrayList<Bridge> plats, double row1_top, double row2_top, double row3_top, double row4_top, double row5_top, Rectangle oilDrum, boolean fireTrigger) {
-        if (y_change<8 && !falling) 
-            y_change += 0.02;
+    public boolean update(ArrayList<Bridge> plats, double row1_top, double row2_top, double row3_top, double row4_top, double row5_top, Rectangle oilDrum) {
+        boolean fireTrigger = false;
+
+        if (y_change < 8 && !falling) 
+            y_change += 0.5; //0.02, 1
 
         for(int i=0; i<plats.size(); i++) {
-            if (this.bottom.getBoundsInParent().intersects(plats.get(i).getTop().getBoundsInParent())) {
+            if (bottom.getBoundsInParent().intersects(plats.get(i).getTop().getBoundsInParent())) {
                 y_change = 0;
-                this.falling = false;
+                falling = false;
             }
         }
 
-        if (this.bottom.getBoundsInParent().intersects(oilDrum.getBoundsInParent())) {
+        if (bottom.getBoundsInParent().intersects(oilDrum.getBoundsInParent())) {
             if (!oilCollision) {
                 oilCollision = true;
-                if (new Random().nextInt(4) == 4) {
+                if (new Random().nextInt(2) == 0) {
                     fireTrigger = true;
                 }
             }
         }
 
         if (!falling) {
-            double rectBottom = this.bottom.getLayoutY() + barrelWidth;
-            if (row5_top >= rectBottom || (row3_top >= rectBottom && rectBottom >= row4_top) || (row1_top >= rectBottom && rectBottom >= row2_top)) {
-                x_change = 0.5;
+            double rectBottom = bottom.getLayoutY() + barrelWidth;
+            if (row5_top > rectBottom || (row3_top > rectBottom && rectBottom > row4_top) || (row1_top > rectBottom && rectBottom > row2_top)) {
+                x_change = 3; //0.5, 3
             } else {
-                x_change = -0.5;
+                x_change = -3;
             }
         } else {
             x_change = 0;
         }
+
         barrel.setX(barrel.getX() + x_change);
         barrel.setY(barrel.getY() + y_change);
-        this.bottom.setLayoutX(this.bottom.getLayoutX() + x_change);
-        this.bottom.setLayoutY(this.bottom.getLayoutY() + y_change);
+        bottom.setLayoutX(bottom.getLayoutX() + x_change);
+        bottom.setLayoutY(bottom.getLayoutY() + y_change);
         
         if (count < 15)
             count += 1;
         else {
             count = 0;
             if (x_change > 0)
-                if (pos < 4)
-                    pos += 0.1;
+                if (pos < 3) //4
+                    pos += 0.5; //0.1, 1
                 else
                     pos = 0;
             else
                 if (pos > 0)
-                    pos -= 0.1;
+                    pos -= 0.5; 
                 else
-                    pos = 4;
+                    pos = 3;
         }
+
+        // Rotate the barrel
+        barrel.setRotate(90 * pos);  
+
+        // Destroy the barrel
+        // if (bottom.getLayoutY() > App.height) {
+        //     System.out.println("barrel out");
+        // }
 
         return fireTrigger;
     }
 
-    public void checkFall(ArrayList<Ladder> lads) {
+    public void checkFall(ArrayList<Ladder> lads, Group root) {
         boolean alreadyCollided = false;
-        Rectangle below = new Rectangle(this.bottom.getLayoutX(), this.bottom.getLayoutY() );
+        Line below = new Line(bottom.getLayoutX() + barrelWidth, bottom.getLayoutY() + barrelHeight, bottom.getLayoutX() + barrelWidth, bottom.getLayoutY() + barrelHeight);
+        below.setStroke(Color.GREEN); 
+        below.setStrokeWidth(3);
+        root.getChildren().add(below);
         for (Ladder lad:lads) {
-            if (below.getBoundsInParent().intersects(lad.getBody().getBoundsInParent()) && !falling && !checkLad) {
-                checkLad = true;
-                alreadyCollided = true;
-                // System.out.println(this.bottom.getLayoutX() + " " + this.bottom.getLayoutY());
-                if (new Random().nextInt(1) == 1) {
-                    falling = true;
-                    y_change = 4;
+            if (lad.getLength() >= 3) {
+                Rectangle ladBody = lad.getBody();
+                Line ladTopLine = new Line(ladBody.getX(), ladBody.getY(), ladBody.getX() + App.section_width, ladBody.getY());
+                Line ladBottomLine = new Line(ladBody.getX(), ladBody.getY() + ladBody.getHeight(), ladBody.getX() + App.section_width, ladBody.getY() + ladBody.getHeight());
+                ladBottomLine.setStroke(Color.BLUE); 
+                ladBottomLine.setStrokeWidth(3);
+                root.getChildren().addAll(ladTopLine, ladBottomLine);
+
+                // ladBody.setFill(Color.RED);   // change color to ckeck its position
+                
+                if (below.getBoundsInParent().intersects(ladTopLine.getBoundsInParent()) && !falling && !checkLad) {
+                    checkLad = true;
+                    alreadyCollided = true;
+                    int rand = new Random().nextInt(5);
+                    System.out.println(below.getBoundsInParent().intersects(ladBody.getBoundsInParent()));
+                    if (true) {
+                        falling = true;
+                        Timeline barrelFaTimeline = new Timeline(new KeyFrame(Duration.millis(5), event -> {
+                            y_change += 100;
+                        }));
+                        // barrelFaTimeline.setCycleCount(Timeline.INDEFINITE); // Repeat indefinitely
+                        barrelFaTimeline.play();
+                        barrel.setY(barrel.getY() + y_change);
+                        bottom.setLayoutY(bottom.getLayoutY() + y_change);
+                    }
                 }
             }
         }
-
-        if (!alreadyCollided) 
-            checkLad = false;
+            if (!alreadyCollided) 
+               checkLad = false;
     }
 
-    public void rotate() {
-        // Set the rotation based on the 'pos' variable
-        barrel.setRotate(90 * pos);       
+
+    public void clear() {
+        barrel.setImage(null);
     }
 
-    // Add getters and setters for other properties as needed
 }
