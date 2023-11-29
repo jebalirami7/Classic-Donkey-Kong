792d95c662cfb5b9f46760f1672a8b928711e9d6
Rami Jebali <jebalirami7@gmail.com>
2023-11-29 20:57:00 +0100

    Add player controlls

--- src/main/java/com/game/Partie.java
+++ src/main/java/com/game/Partie.java
@@ -7,15 +7,20 @@ import java.util.Random;
 import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
+import javafx.util.Pair;
 
 public class Partie {
     // ImageViews
@@ -24,9 +29,8 @@ public class Partie {
     ImageView dk = new ImageView();
     ImageView peach = new ImageView(App.peach2);
 
-
     // Initialize bridges rows
-    private int start_y = App.height - 2 * App.section_height;
+    private int start_y = (int)App.height - 2 * App.section_height;
     private int row2_y = start_y - 4 * App.section_height;
     private int row3_y = row2_y - 7 * App.slope - 3 * App.section_height;
     private int row4_y = row3_y - 4 * App.section_height;
@@ -39,7 +43,7 @@ public class Partie {
     private int row2_top = row2_y - 8 * App.slope;
     private int row1_top = start_y - 5 * App.slope;
 
-    private boolean fireBall = false;
+    private boolean fireBallTrigger = true;
 
     private int counter = 0;
 
@@ -48,14 +52,17 @@ public class Partie {
 
     List<Level> levels;
 
-    private final int barrelSpawnTime = 3600;
+    private final int barrelSpawnTime = 360;   //3600, 1000
     private int barrelCount = barrelSpawnTime / 2;
-    private int barrelTime = 2000;
+    private int barrelTime = 180;   //2000, 500
     private ArrayList<Bridge> bridge_objs = new ArrayList<Bridge>();
     private ArrayList<Ladder> ladder_objs = new ArrayList<Ladder>();
+    private ArrayList<Hammer> hammer_objs = new ArrayList<Hammer>();
     private List<Barrel> barrels = new ArrayList<>();
+    private List<FireBall> fireBalls = new ArrayList<>();
+    private Rectangle oilDrum;
 
-    private Rectangle oilDrum = new Rectangle(1,1);
+    Pair<Boolean, Boolean> climbingStatus = new Pair<>(false, false);
 
 
     public Partie() {
@@ -65,10 +72,10 @@ public class Partie {
     }
 
 
-    void createPartie(Group root, GraphicsContext gc) {
+    void createPartie(Group root, GraphicsContext gc, Scene scene) {
         
         // Draw Extras
-        drawOil(root);
+        oilDrum = drawOil(root);
         drawBarrels(root);
 
         // Create level 1 
@@ -111,11 +118,16 @@ public class Partie {
                 List.of(13, row6_y + 5 * App.slope, 2), List.of(13, row6_y + 25 * App.slope, 2),
                 List.of(18, row6_y - 27 * App.slope, 4), List.of(12, row6_y - 17 * App.slope, 2),
                 List.of(10, row6_y - 17 * App.slope, 2), List.of(12, -5, 13), List.of(10, -5, 13)
+            ),
+            List.of(
+                List.of(3 * App.section_width, row6_top + App.section_height), 
+                List.of(3 * App.section_width, row2_top + App.section_height)
             )
         );
 
         ladder_objs = lvl1.createLadders(root);
         bridge_objs = lvl1.createBridges(root);
+        hammer_objs = lvl1.createHammers(root);
 
         // Draw Kong
         dk.setCache(true);
@@ -138,18 +150,25 @@ public class Partie {
         
         root.getChildren().addAll(barrelImg, dk, peach);
         
-        AnimationTimer gameLoop = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                renderGame(gc, root);
-            }
-        };
-        gameLoop.start();
+        // AnimationTimer gameLoop = new AnimationTimer() {
+            //     @Override
+            //     public void handle(long now) {
+                //         renderGame(gc, root);
+                //     }
+                // };
+                // gameLoop.start();
+                
+        Player player = new Player(7 * App.section_width, App.height - 6 * App.section_height, root);
+
+        Timeline fireTimeLine = new Timeline(new KeyFrame(Duration.millis(15), event -> {renderGame(gc, root, scene, player);}));
+        fireTimeLine.setCycleCount(Timeline.INDEFINITE); // Repeat indefinitely
+        fireTimeLine.play();
         
+
     }
     
     
-    private void renderGame(GraphicsContext gc, Group root) {
+    private void renderGame(GraphicsContext gc, Group root, Scene scene, Player player) {
         // Draw Background
         gc.setFill(javafx.scene.paint.Color.BLACK);
         gc.fillRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
@@ -157,26 +176,111 @@ public class Partie {
         if (barrelCount < barrelSpawnTime) {
             barrelCount++;
         } else {
-            barrelCount = new Random().nextInt(360, 1200);
+            barrelCount = new Random().nextInt(180);
             barrelTime = barrelSpawnTime - barrelCount;
             Barrel barrel = new Barrel(270, 250, root);
             barrels.add(barrel);
         }
-        
+
         for (Barrel barrel : barrels) {
-            barrel.checkFall(ladder_objs);
-            fireBall = barrel.update(bridge_objs, row1_top, row2_top, row3_top, row4_top, row5_top, oilDrum, fireBall);
-        }
-        animateKong(root);
-        for (Barrel barrel : barrels) {
-            barrel.rotate();
+            // barrel.checkFall(ladder_objs, root);   // feha mochkla sghira
+            fireBallTrigger = barrel.update(bridge_objs, row1_top, row2_top, row3_top, row4_top, row5_top, oilDrum);
+            if (fireBallTrigger) {
+                barrel.clear();
+                FireBall fireBall = new FireBall(5 * App.section_width, App.height - 4 * App.section_height, root);
+                fireBalls.add(fireBall);
+                fireBallTrigger = false;
+            }
         }
 
+        for (FireBall fireball : fireBalls) {
+            fireball.checkFall(ladder_objs, row2_y, row3_y, row4_y, row5_y, row6_y);
+            fireball.update(bridge_objs);
+        }
+
+        for (Hammer hammer : hammer_objs) {
+            hammer.draw(player);
+        }
+
+        animateKong(root);
+
+        player.update(bridge_objs);
+        player.draw();
+        climbingStatus = player.checkClimb(ladder_objs);
+
+        keyboardManager(scene, player, climbingStatus.getKey(), climbingStatus.getValue());
+
+        System.out.println(climbingStatus);
+        
+    }
+
+
+    public void keyboardManager(Scene scene, Player player, boolean climb, boolean down) {
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent e) {
+                KeyCode key = e.getCode();
+
+                if ((key == KeyCode.RIGHT || key == KeyCode.D) && !player.isClimbing() ) {
+                    System.out.println("moving right");
+                    player.setX_change(1);
+                    player.setDir(1);
+                }
+                if ((key == KeyCode.LEFT || key == KeyCode.Q) && !player.isClimbing() ) {
+                    System.out.println("moving left");
+                    player.setX_change(-1);
+                    player.setDir(-1);
+                }
+                if (key == KeyCode.SPACE && player.isLanded() && !player.getHammer()) {
+                    System.out.println("jumping");
+                    player.setLanded(false);
+                    player.setY_change(-5.5);
+                }
+                if ((key == KeyCode.UP || key == KeyCode.Z) && !player.getHammer()) {
+                    if (climb) {
+                        System.out.println("climbing up");
+                        player.setY_change(-2);
+                        player.setX_change(0);
+                        player.setClimbing(true);
+                    }
+                }
+                if ((key == KeyCode.DOWN || key == KeyCode.S) && !player.getHammer()) {
+                    if (down) {
+                        System.out.println("climbing down");
+                        player.setY_change(2);
+                        player.setX_change(0);
+                        player.setClimbing(true);
+                    }
+                }
+            }
+        });
+
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent e) {
+                KeyCode key = e.getCode();
+
+                if (key == KeyCode.RIGHT || key == KeyCode.D)  {
+                    player.setX_change(0);
+                }
+                if (key == KeyCode.LEFT || key == KeyCode.Q) {
+                    player.setX_change(0);
+                }
+                if (key == KeyCode.UP || key == KeyCode.Z || key == KeyCode.DOWN || key == KeyCode.S) {
+                    if (climb) {
+                        player.setY_change(0);
+                    }
+                    if (player.isClimbing() && player.isLanded()) {
+                        player.setClimbing(false);
+                    }
+                }             
+            }
+        });
     }
 
 
     public void drawBarrels(Group root) {
-        // Draw 4 Barrels On The Upper-Left Corner
+        // Draw 4 Barrels On The Top-Left Corner
         List<Double> dx = List.of(1.2, 1.2, 2.5, 2.5);
         List<Double> dy = List.of(5.4, 7.7, 7.7, 5.4);
         for(int i=0; i<4 ;i++) {
@@ -219,7 +323,7 @@ public class Partie {
 
         // Draw Oil Drum 
         Rectangle oil = new Rectangle(x_coord, y_coord, 2 * App.section_width, 2.5 * App.section_height);
-        oil.setFill(Color.BLUE);   // change color to ckeck its position
+        oil.setFill(Color.BLUE);  
 
         // Draw Oil Draw Reflexions
         Rectangle oilTop = new Rectangle(x_coord - 0.1 * App.section_width, y_coord, 2.2 * App.section_width, .2 * App.section_height);
@@ -271,7 +375,6 @@ public class Partie {
                 counter++;
             else counter =0;
         }));
-
         fireTimeLine.setCycleCount(Timeline.INDEFINITE); // Repeat indefinitely
         fireTimeLine.play();
         
