27104aa1987b9c0d826eb789b11d2051c7694737
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2023-12-04 09:40:19 +0100

    Change paths to relative

--- src/main/java/com/game/App.java
+++ src/main/java/com/game/App.java
@@ -1,5 +1,6 @@
 package main.java.com.game ;
 
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -12,25 +13,27 @@ import javafx.stage.Stage;
 public class App extends Application {
 
     //Images
-    public static Image barrelImg = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/barrels/barrel.png");
-    public static Image barrel2 = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/barrels/barrel2.png");
-    public static Image fireImg = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/fire.png");
-    public static Image dk1 = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/dk/dk1.png");
-    public static Image dk2 = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/dk/dk2.png");
-    public static Image dk3 = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/dk/dk3.png");
-    public static Image peach1 = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/peach/peach1.png");
-    public static Image peach2 = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/peach/peach2.png");
-    public static Image fireBallImg = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/fireball.png");
-    public static Image fireBall2 = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/fireball2.png");
-    public static Image hammerImg = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/hammer.png");
-    public static Image standing = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/mario/standing.png");
-    public static Image running = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/mario/running.png");
-    public static Image jumping = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/mario/jumping.png");
-    public static Image climbing1 = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/mario/climbing1.png");
-    public static Image climbing2 = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/mario/climbing2.png");
-    public static Image hammerJump = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/mario/hammer_jump.png");
-    public static Image hammerStand = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/mario/hammer_stand.png");
-    public static Image hammerOverhead = new Image("file:/home/rami/Desktop/donkey-kong/src/main/resources/assets/images/mario/hammer_overhead.png");
+
+
+    public static Image barrelImg = new Image( "file:src/main/resources/assets/images/barrels/barrel.png");
+    public static Image barrel2 = new Image("file:src/main/resources/assets/images/barrels/barrel2.png");
+    public static Image fireImg = new Image("file:src/main/resources/assets/images/fire.png");
+    public static Image dk1 = new Image("file:src/main/resources/assets/images/dk/dk1.png");
+    public static Image dk2 = new Image("file:src/main/resources/assets/images/dk/dk2.png");
+    public static Image dk3 = new Image("file:src/main/resources/assets/images/dk/dk3.png");
+    public static Image peach1 = new Image("file:src/main/resources/assets/images/peach/peach1.png");
+    public static Image peach2 = new Image("file:src/main/resources/assets/images/peach/peach2.png");
+    public static Image fireBallImg = new Image("file:src/main/resources/assets/images/fireball.png");
+    public static Image fireBall2 = new Image("file:src/main/resources/assets/images/fireball2.png");
+    public static Image hammerImg = new Image("file:src/main/resources/assets/images/hammer.png");
+    public static Image standing = new Image("file:src/main/resources/assets/images/mario/standing.png");
+    public static Image running = new Image("file:src/main/resources/assets/images/mario/running.png");
+    public static Image jumping = new Image("file:src/main/resources/assets/images/mario/jumping.png");
+    public static Image climbing1 = new Image("file:src/main/resources/assets/images/mario/climbing1.png");
+    public static Image climbing2 = new Image("file:src/main/resources/assets/images/mario/climbing2.png");
+    public static Image hammerJump = new Image("file:src/main/resources/assets/images/mario/hammer_jump.png");
+    public static Image hammerStand = new Image("file:src/main/resources/assets/images/mario/hammer_stand.png");
+    public static Image hammerOverhead = new Image("file:src/main/resources/assets/images/mario/hammer_overhead.png");
 
     // public static final double width = Screen.getPrimary().getBounds().getWidth() - Screen.getPrimary().getBounds().getWidth() * 0.4;
     // public static final double height = Screen.getPrimary().getBounds().getHeight() - Screen.getPrimary().getBounds().getHeight() * 0.1;
